e69dc8b5ed9d1b2022c5f913b48eac725972ef89
Renzo <rcnavarrop@gmail.com>
2019-09-06 22:27:38 -0500

    testing sequelize

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import db from './conn/db';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("This is a test of end point");
+  res.send('This is a test of end point');
 });
 
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.listen(PORT, () => {
-    console.log("Environment variables set: ", process.env.SECRET_TEST);
-    console.log(`Express server listening on port ${PORT}`)
-})
+  console.log('Environment variables set: ', process.env);
+  console.log(`Express server listening on port ${PORT}`);
+});

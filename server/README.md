8c8c53a8a51e24e8c7f228deef46508e7fd12ba8
Renzo <rcnavarrop@gmail.com>
2019-09-06 19:06:07 -0500

    setting basic express server

---
+++ server/README.md
@@ -0,0 +1,6 @@
+# backend-general-setup
+This is a general setup to start a backend project. It includes the following packages:
+
+- `Nodemon`: For automatic restart of server after a change
+- `Babel`: Transpiler for ES6
+- `Express`: As web server
---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("This is a test of end point");
+});
+
+app.listen(PORT, () => {
+    console.log("Environment variables set: ", process.env.SECRET_TEST);
+    console.log(`Express server listening on port ${PORT}`)
+})

6d1d5ffef9f285d411fa9ce5459ac0fb265ad0ba
Renzo <rcnavarrop@gmail.com>
2019-09-09 10:37:00 -0500

    helper function for deliveryTime and shipping cost created

--- server/client/src/assets/helperFunctions.js
+++ server/client/src/assets/helperFunctions.js
@@ -1,20 +1,46 @@
+const getShippingCost = productsCost => {
+  let shippingCost = productsCost * 0.1;
+  return shippingCost;
+};
+
+const getTaxes = productsCost => {
+  let taxes = productsCost * 0.18;
+  return taxes;
+};
+
+const getNextWorkingDate = (date, hourOffset) => {
+  let nextWorkingDate = date.setHours(date.getHours() + hourOffset);
+  return nextWorkingDate;
+};
+
+const getDateFormat = date => {
+  let deliveryDate = new Date(date).toJSON().split('-');
+  let day = deliveryDate[2].slice(0, 2);
+  let month = deliveryDate[1];
+  let year = deliveryDate[0].slice(2, 4);
+  return `${month}/${day}/${year}`;
+};
+
 export const getDeliveryTime = () => {
   let currentDate = new Date();
   let day = currentDate.getDay();
   let deliveryDate = '';
 
   if (day === 5) {
-    deliveryDate = currentDate.setHours(currentDate.getHours() + 72);
+    deliveryDate = getNextWorkingDate(currentDate, 72);
   }
   if (day === 6) {
-    deliveryDate = currentDate.setHours(currentDate.getHours() + 48);
+    deliveryDate = getNextWorkingDate(currentDate, 48);
   } else {
-    deliveryDate = currentDate.setHours(currentDate.getHours() + 24);
+    deliveryDate = getNextWorkingDate(currentDate, 24);
   }
-  deliveryDate = new Date(deliveryDate).toJSON().split('-');
-  deliveryDate = `${deliveryDate[1]}/${deliveryDate[2].slice(
-    0,
-    2
-  )}/${deliveryDate[0].slice(2, 4)}`;
+  deliveryDate = getDateFormat(deliveryDate);
   return deliveryDate;
+};
+
+export const getQuotation = productsCost => {
+  let taxes = getTaxes(productsCost);
+  let shippingCost = getShippingCost(productsCost);
+  let totalCost = productsCost + shippingCost;
+  return { totalCost, shippingCost, taxes };
 };

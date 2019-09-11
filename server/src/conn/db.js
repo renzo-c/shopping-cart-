9bc4aa0fc1e73657e6708a07ac3b6ecc3fbfa0fd
Renzo <rcnavarrop@gmail.com>
2019-09-11 02:22:16 -0500

    missing type

--- server/src/conn/db.js
+++ server/src/conn/db.js
@@ -2,7 +2,7 @@ import Sequelize from 'sequelize';
 import productModel from './product';
 import orderModel from './order';
 import productOrder from './productOrder';
-require('dotenv').config();
+// require('dotenv').config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,

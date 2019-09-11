6529d451febfcc48d4d5629b46528fe4110902a8
Renzo <rcnavarrop@gmail.com>
2019-09-10 23:57:17 -0500

    commenting dotenv imports for development purposes

--- server/src/conn/db.js
+++ server/src/conn/db.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import productModel from './product';
 import orderModel from './order';
-require("dotenv").config();
+// require("dotenv").config();
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
